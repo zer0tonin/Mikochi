df878601e57bacea605db48f2f51a3a76732d551
Alice GG <hi@alicegg.tech>
2023-01-09 18:23:51 +0100

    Basic folder list

--- src/main.go
+++ src/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/spf13/viper"
 )
+
+var folderTemplate *template.Template
 
 func init() {
 	viper.SetConfigName("config")
@@ -16,6 +21,40 @@ func init() {
 		panic("Failed to load config")
 	}
 
+	folderTemplate = template.Must(template.ParseFiles("./templates/folder.html"))
+}
+
+func readDataDir(path string) (result []string, err error) {
+	if strings.Contains(path, "..") {
+		return result, fmt.Errorf("Invalid path")
+	}
+
+	fmt.Println(viper.GetString("dataDir"))
+	files, err := os.ReadDir(viper.GetString("dataDir") + path)
+	if err != nil {
+		return
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+		result = append(result, file.Name())
+	}
+	return result, nil
+}
+
+func displayFolder(w http.ResponseWriter, path string) error {
+	files, err := readDataDir(path)
+	if err != nil {
+		return err
+	}
+
+	folderTemplate.Execute(
+		w,
+		map[string]interface{}{
+			"files": files,
+		},
+	)
+	return nil
 }
 
 func main() {
@@ -23,7 +62,7 @@ func main() {
 		switch r.Method {
 		case http.MethodGet:
 			if r.URL.Path == "/" {
-				fmt.Fprintf(w, "Hello world\n")
+				displayFolder(w, "")
 			}
 		default:
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
