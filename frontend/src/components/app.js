e780e11df5d6ee8f0c0f9ed26f4b0bfa07aeaf8d
Alice GG <hi@alicegg.tech>
2023-03-31 16:02:26 +0200

    Remove TODO

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -4,7 +4,6 @@ import { Router } from "preact-router";
 // Code-splitting is automated for `routes` directory
 import Directory from "../routes/directory/directory";
 
-// TODO: clean up dead code & prettify
 const App = () => (
   <div id="app">
     <Router>
