4e3c39fa76d1c060752f53b90f5be12a9f92b4dd
Alice GG <hi@alicegg.tech>
2023-05-13 11:56:13 +0200

    Fix copyLink outside of secure contexts

--- frontend/src/components/copylink.js
+++ frontend/src/components/copylink.js
@@ -5,10 +5,38 @@ import { AuthContext } from "../utils/jwt";
 
 import Icon from "./icon";
 
+// copyToClipboard will copy text to the clipboard using navigator.clipboard if available
+// or fallback to document.execCommand
+const copyToClipboard = async (textToCopy) => {
+    // Navigator clipboard api needs a secure context (https)
+    if (navigator.clipboard && window.isSecureContext) {
+        await navigator.clipboard.writeText(textToCopy);
+    } else {
+        // Use the 'out of viewport hidden text area' trick
+        const textArea = document.createElement("textarea");
+        textArea.value = textToCopy;
+            
+        // Move textarea out of the viewport so it's not visible
+        textArea.style.position = "absolute";
+        textArea.style.left = "-999999px";
+            
+        document.body.prepend(textArea);
+        textArea.select();
+
+        try {
+            document.execCommand('copy');
+        } catch (error) {
+            console.error(error);
+        } finally {
+            textArea.remove();
+        }
+    }
+}
+
 const CopyLink = ({ filePath }) => {
   const { jwt } = useContext(AuthContext);
 
-  const copyToClipboard = async () => {
+  const copyWithAuth = async () => {
     const response = await fetch(`/api/single-use`, {
       headers: {
         Accept: "application/json",
@@ -20,13 +48,13 @@ const CopyLink = ({ filePath }) => {
     const params = new URLSearchParams();
     params.append("auth", json["token"]);
 
-    navigator.clipboard.writeText(
+    await copyToClipboard(
       `${window.location.protocol}//${window.location.hostname}${
         window.location.port == "" ? "" : `:${window.location.port}`
       }/api/stream${filePath}?${params.toString()}`
     );
   };
-  return <Icon name="copy" onClick={copyToClipboard} />;
+  return <Icon name="copy" onClick={copyWithAuth} />;
 };
 
 export default CopyLink;
