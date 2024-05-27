217f55a7694ff5835897c6bd29c6cf6097ca1f45
Alice GG <hi@alicegg.tech>
2024-05-27 15:18:51 +0200

    NoAuth middleware

---
+++ backend/auth/noauth_middleware.go
@@ -0,0 +1,18 @@
+package auth
+
+import "github.com/gin-gonic/gin"
+
+type NoauthMiddleware struct {
+}
+
+func (n *NoauthMiddleware) setWhitelist(jti, target string) {
+	return
+}
+
+func (n *NoauthMiddleware) CheckAuth(c *gin.Context) {
+	c.Next()
+}
+
+func (n *NoauthMiddleware) CheckStreamAuth(c *gin.Context) {
+	c.Next()
+}
