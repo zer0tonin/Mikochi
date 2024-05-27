d0adad3e167b5b0f52a3e580b48cfb4a56dfa9b0
Alice GG <hi@alicegg.tech>
2024-05-27 15:18:51 +0200

    NoAuth middleware

---
+++ backend/auth/middleware_factory.go
@@ -0,0 +1,26 @@
+package auth
+
+import (
+	"sync"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AuthMiddleware interface {
+	setWhitelist(jti, target string)
+	CheckAuth(c *gin.Context)
+	CheckStreamAuth(c *gin.Context)
+}
+
+
+func NewAuthMiddleware(withAuth bool, jwtSecret string) AuthMiddleware {
+	if (withAuth) {
+		return &JwtMiddleware{
+			jwtSecret: []byte(jwtSecret),
+			tokenWhitelist: map[string]string{},
+			tokenWhitelistMutex: sync.Mutex{},
+		}
+	}
+	return &NoauthMiddleware{}
+}
+
