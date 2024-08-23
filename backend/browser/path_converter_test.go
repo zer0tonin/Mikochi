5a531fe7fbe19cb0f6db3b6ef26965cac44ea8c5
Alice GG <hi@alicegg.tech>
2024-08-23 13:39:33 +0200

    Add tests for path_converter + edge cases

---
+++ backend/browser/path_converter_test.go
@@ -0,0 +1,42 @@
+package browser
+
+import (
+	"testing"
+)
+
+func TestGetAbsolutePath(t *testing.T) {
+	tests := []struct{
+		name string
+		dataDir string
+		path string
+		expected string
+	}{
+		{
+			name: "Empty path",
+			dataDir: "/data",
+			path: "",
+			expected: "/data",
+		},
+		{
+			name: "Two sub dirs",
+			dataDir: "/data",
+			path: "/sub/nested",
+			expected: "/data/sub/nested",
+		},
+		{
+			name: "Unclean path",
+			dataDir: "/data",
+			path: "/sub/nested/..",
+			expected: "/data/sub",
+		},
+	}
+
+	for _, test := range tests {
+		pathConverter := NewPathConverter(test.dataDir)
+		actual := pathConverter.GetAbsolutePath(test.path)
+
+		if actual != test.expected {
+			t.Errorf("Invalid absolute path, expected %s, got %s", test.expected, actual)
+		}
+	}
+}
