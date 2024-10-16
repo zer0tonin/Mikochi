f3453b9390dd710a229a6662c0521b53f22a065a
Anh Chien Vu <vuanhchien003@gmail.com>
2024-10-16 12:58:44 -0400

    get jwt from AuthContext

--- frontend/src/components/logout/index.jsx
+++ frontend/src/components/logout/index.jsx
@@ -1,31 +1,33 @@
 import { h } from "preact";
+import React, { useContext } from "react";
+import { AuthContext } from "../../jwt";
 
 import { useLocation } from "preact-iso";
 import "./style.css";
 
 const Logout = () => {
   const location = useLocation();
+  const { jwt } = useContext(AuthContext);
+
   const handleLogout = async () => {
     try {
-      const token = localStorage.getItem("jwt");
-      if (!token) {
-        console.error("No token found in local storage");
+      if (!jwt) {
+        console.error("No token found in context");
         return;
       }
       await fetch("/api/logout", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${jwt}`,
         },
       });
 
-      localStorage.removeItem("jwt");
-
-      //   location.route("/");
-      window.location.href = "/";
+      location.route("/");
     } catch (error) {
       console.error("Error logging out:", error);
+    } finally {
+      localStorage.removeItem("jwt");
     }
   };
 
