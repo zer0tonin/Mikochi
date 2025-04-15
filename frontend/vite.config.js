4a20614ca28b2cc610296454f22500471f952b76
Alice GG <hi@alicegg.tech>
2025-04-15 16:45:18 +0200

    Fix vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import preact from "@preact/preset-vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  server: {
+    allowedHosts: ["dev-frontend"],
+  },
 });
