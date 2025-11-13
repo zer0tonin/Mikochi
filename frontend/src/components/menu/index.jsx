c45e99b9f62578cdbdab346498e939a26e80aecb
Alice GG <hi@alicegg.tech>
2025-11-13 16:51:42 +0100

    Close mennu on click outside

--- frontend/src/components/menu/index.jsx
+++ frontend/src/components/menu/index.jsx
@@ -1,21 +1,41 @@
 import { h } from "preact";
 import "./style.css";
-import { useState } from "preact/hooks";
+import { useEffect, useRef, useState } from "preact/hooks";
 import Icon from "../icon";
 import LogOut from "./logout";
-import {mkdirOpen} from "../add/mkdir";
-import {uploadOpen} from "../add/upload";
+import { mkdirOpen } from "../add/mkdir";
+import { uploadOpen } from "../add/upload";
 
 const Menu = ({ onHomeClick }) => {
   const [visible, setVisible] = useState(false);
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setVisible(false);
+      }
+    };
+
+    if (visible) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [visible]);
+
   return (
-    <span class="menu" onClick={() => setVisible(!visible)}>
+    <span ref={menuRef} class="menu" onClick={() => setVisible(!visible)}>
       <Icon name="menu" />
       {visible && (
         <ul class="dropdown-content">
           <li onClick={onHomeClick}>Home</li>
-          <li onClick={() => mkdirOpen.value = true}>New directory</li>
-          <li onClick={() => uploadOpen.value = true}>Upload</li>
+          <li onClick={() => (mkdirOpen.value = true)}>New directory</li>
+          <li onClick={() => (uploadOpen.value = true)}>Upload</li>
           <LogOut />
         </ul>
       )}
